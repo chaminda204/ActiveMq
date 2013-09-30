ac9a96bfb9e6fdd9991f10bbf2811a1580a4a098
chaminda204 <chaminda204@yahoo.co.uk>
2013-09-30 23:25:51 +0800

    adding listeners to process messages based on the state

---
+++ src/main/java/com/chaminda/activemq/listener/WAProductMessageListener.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.chaminda.activemq.listener;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+
+import org.springframework.stereotype.Component;
+
+import com.chaminda.activemq.domain.Product;
+
+/**
+ * @author chaminda
+ *         <p/>
+ *         date 08/09/2013
+ */
+@Component
+public class WAProductMessageListener implements MessageListener {
+
+	public void onMessage(final Message message) {
+		
+		if (message instanceof ObjectMessage) {
+			
+			final ObjectMessage objectMessage = (ObjectMessage) message;
+			
+			try {
+				
+				Product product = (Product) objectMessage.getObject();
+				System.out.println( product.getProductID()+ " Product has been processed to WA standards");
+			
+			} catch (JMSException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+}
